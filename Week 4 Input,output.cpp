b5887d62a67f29c7bec5c6d460ecba326c6aef88
Cparra001 <Cparra001@student.butte.edu>
2025-02-16 14:47:36 -0800

    Create Week 4 Input,output.cpp

---
+++ Week 4 Input,output.cpp
@@ -0,0 +1,19 @@
+//Created by Carlos Parra
+//Date is 2/16/2025
+//This is a debugging program to solve a problem
+
+#include <iostream>
+springname std;
+int() {
+  cout<<"2"<<"\n"
+  cout<<"4"<<"\n"
+  cout<<"8"<<"\n"
+  Program
+  cout<<"A=2"\n"
+  cout<<"print A"\n"
+    A=A*2
+  cout<<"print A"\n"
+    A=A*2
+  cout<<"print A"\n"
+    A=100
+    }
